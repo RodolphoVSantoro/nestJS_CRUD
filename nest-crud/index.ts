f555a03705e5250c5a422d962efe5422dc7c9f74
RodolphoVSantoro <rodolphovs@gmail.com>
2022-10-12 09:09:17 -0300

    feat: pubsub test1

--- nest-crud/index.ts
+++ nest-crud/index.ts
@@ -1,3 +1,4 @@
+/*
 import { NestFactory } from '@nestjs/core';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import express from 'express';
@@ -16,7 +17,6 @@ const createFunction = async (expressInstance): Promise<void> => {
         methods: 'POST',
     };
     app.enableCors(corsOptions);
-    */
     await app.init();
 };
 
@@ -24,3 +24,47 @@ export const api = functions.region('southamerica-east1').https.onRequest(async 
     await createFunction(expressServer);
     expressServer(request, response);
 });
+*/
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const {PubSub} = require("@google-cloud/pubsub");
+
+const pubsub = new PubSub();
+admin.initializeApp();
+const database = admin.database();
+
+
+exports.trigger = functions.https.onRequest((request, response) => {
+    const messageBody = JSON.stringify({sendAt: Date.now()});
+    const buffer = Buffer.from(messageBody);
+    const fullTopicName = "totvs-integration/test";
+
+    return pubsub
+        .topic(fullTopicName)
+        .publish(buffer)
+        .then((messageId) => {
+            console.log(`Message ${messageId} published to topic ${fullTopicName}`);
+            response.send(JSON.stringify({messageId}));
+        })
+        .catch((error) => {
+            console.error(`unable to publish to topic ${fullTopicName}`);
+        });
+});
+
+exports.run = functions.pubsub.topic("test")
+    .onPublish((message) => {
+        const {sentAt} = message.json;
+        console.log(`triggered at ${new Date(sentAt).toString()}`);
+        // Perform time-consuming task
+        return new Promise((resolve) => {
+            let progress = 0;
+            const handler = setInterval(() => {
+                progress += 1;
+                database.ref("progress").set(progress);
+                if (progress >= 100) {
+                    clearInterval(handler);
+                    resolve(1);
+                }
+            }, 500);
+        });
+    });
